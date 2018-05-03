c0c79bcfcfcb1fedacd19469169ae311fa2620b5
Dylan Vann <dylan.vann@gmail.com>
2018-05-02 22:44:40 -0400

    Remove unused scripts.

--- renew-cert.sh
+++
@@ -1,5 +0,0 @@
-sudo certbot certonly \
-    --manual \
-    -d dylanvann.com \
-    -d www.dylanvann.com
-
--- upload-cert.sh
+++
@@ -1,7 +0,0 @@
-sudo aws iam upload-server-certificate \
-    --server-certificate-name cert_dylanvann_2 \
-    --certificate-body file:///etc/letsencrypt/live/dylanvann.com/cert.pem \
-    --private-key file:///etc/letsencrypt/live/dylanvann.com/privkey.pem \
-    --certificate-chain file:///etc/letsencrypt/live/dylanvann.com/chain.pem \
-    --path /cloudfront/certs/
-
