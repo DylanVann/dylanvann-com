c0b172c27b9faa47863ca45fe53fb02803d30a56
Dylan Vann <dylan@dylanvann.com>
2018-07-10 00:22:49 -0400

    Restructuring.

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,54 +3,60 @@ const Promise = require('bluebird')
 const path = require('path')
 const { createFilePath } = require('gatsby-source-filesystem')
 
-exports.createPages = ({ graphql, actions }) => {
+exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
+  const blogPost = path.resolve('./src/templates/blog-post.js')
 
-  return new Promise((resolve, reject) => {
-    const blogPost = path.resolve('./src/templates/blog-post.js')
-    resolve(
-      graphql(
-        `
-          {
-            allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }, limit: 1000) {
-              edges {
-                node {
-                  fields {
-                    slug
-                  }
-                  frontmatter {
-                    title
-                  }
-                }
+  const result = await graphql(
+    `
+      {
+        allMarkdownRemark(
+          sort: { fields: [frontmatter___date], order: DESC }
+          limit: 1000
+        ) {
+          edges {
+            node {
+              fields {
+                slug
+              }
+              frontmatter {
+                title
               }
             }
           }
-        `
-      ).then(result => {
-        if (result.errors) {
-          console.log(result.errors)
-          reject(result.errors)
         }
+      }
+    `
+  )
 
-        // Create blog posts pages.
-        const posts = result.data.allMarkdownRemark.edges;
+  if (result.errors) {
+    console.log(result.errors)
+    reject(result.errors)
+  }
 
-        _.each(posts, (post, index) => {
-          const previous = index === posts.length - 1 ? null : posts[index + 1].node;
-          const next = index === 0 ? null : posts[index - 1].node;
+  // Create blog posts pages.
+  const allEntries = result.data.allMarkdownRemark.edges.map(v => v.node)
+  const posts = allEntries.filter(e => e.fields.slug.startsWith('/blog/'))
+  const pages = allEntries.filter(e => !e.fields.slug.startsWith('/blog/'))
+  console.log('----------')
+  console.log(allEntries)
+  console.log('----------')
+  console.log(posts)
+  console.log('----------')
 
-          createPage({
-            path: post.node.fields.slug,
-            component: blogPost,
-            context: {
-              slug: post.node.fields.slug,
-              previous,
-              next,
-            },
-          })
-        })
-      })
-    )
+  _.each(posts, (post, index) => {
+    const previous = index === posts.length - 1 ? null : posts[index + 1].node
+    const next = index === 0 ? null : posts[index - 1].node
+
+    createPage({
+      path: post.fields.slug,
+      component: blogPost,
+      context: {
+        slug: post.fields.slug,
+        previous,
+        next,
+      },
+    })
   })
 }
 
