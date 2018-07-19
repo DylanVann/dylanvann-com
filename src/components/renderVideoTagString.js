9719ea625f0263c260fe27d85b76742bc0642dd0
Dylan Vann <dylan@dylanvann.com>
2018-07-18 23:39:54 -0400

    Fix gifs on ios.

--- src/components/renderVideoTagString.js
+++ src/components/renderVideoTagString.js
@@ -1,7 +1,8 @@
 // React does not render the <video> element correctly.
 // https://github.com/facebook/react/issues/6544
 const renderVideoTagString = ({ srcVideo, srcVideoPoster }) => `
-<video muted autoplay playsinline loop poster="${srcVideoPoster}" src="${srcVideo}">
+<video muted autoplay playsinline loop poster="${srcVideoPoster}">
+  <source src="${srcVideo}" type="video/mp4">
 </video>
 `
 
