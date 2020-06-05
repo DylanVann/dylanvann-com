fa9568445445191d319a22fcf0d1b1cef63d4ebe
Dylan Vann <dylan@dylanvann.com>
2020-06-04 21:54:06 -0400

    feat: add newsletter form

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -1,9 +1,13 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
 import React from 'react'
 import { A } from './Markdown'
 // @ts-ignore
 import ProfileImage from './ProfileImage.jpg'
 import { rhythm } from '../typography'
 import styled from '@emotion/styled'
+import { GitHubLink } from './GitHubLink'
+import { Stack } from './Stack'
 
 const Container = styled('div')`
   display: flex;
@@ -18,29 +22,112 @@ const ProfilePicture = styled('img')`
   border-radius: 50%;
 `
 
+const Button = GitHubLink.withComponent('button')
+
 export const Focus = () => (
-  <>
+  <React.Fragment>
     Focused on <strong>Node</strong> | <strong>GraphQL</strong> |{' '}
     <strong>React</strong> | <strong>React Native</strong>.
-  </>
+  </React.Fragment>
 )
 
-export const Bio = () => (
-  <Container>
-    <ProfilePicture src={ProfileImage} alt={`Dylan Vann`} />
-    <div>
-      <p>
-        <strong>Dylan Vann</strong>
+const SignUpForm = () => {
+  const [name, setName] = React.useState('')
+  const [email, setEmail] = React.useState('')
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault()
+        const formId = 1438911
+        fetch(`https://api.convertkit.com/v3/forms/${formId}/subscribe`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            api_key: '2jnVrugk1S7PzsMPVIhMVg',
+            first_name: name,
+            email,
+          }),
+        })
+      }}
+      sx={{
+        display: 'grid',
+        gap: 1,
+        marginLeft: '5%',
+        marginRight: '5%',
+        flex: 1,
+      }}
+    >
+      <strong sx={{ marginBottom: 1 }}>Join the Newsletter</strong>
+      <div>
+        <label htmlFor="firstName" sx={{ marginRight: 10 }}>
+          First Name
+        </label>
         <br />
-        Software developer living in Toronto 🇨🇦 (and sometimes NYC 🗽).
+        <input
+          name="firstName"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          sx={{ width: '100%' }}
+        />
+      </div>
+      <div sx={{ marginBottom: 10 }}>
+        <label htmlFor="email" sx={{ marginRight: 10 }}>
+          Email
+        </label>
         <br />
-        <Focus />
-      </p>
-      <p>
-        <A href="https://twitter.com/atomarranger">
-          Follow me on Twitter for more stuff like this.
-        </A>
-      </p>
-    </div>
-  </Container>
-)
+        <input
+          required
+          name="email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          sx={{ width: '100%' }}
+        />
+      </div>
+      <Button
+        type="submit"
+        sx={{
+          backgroundColor: '#428bca',
+          color: 'white !important',
+          border: 'none',
+          ':hover': {
+            color: 'white !important',
+            backgroundColor: '#1f496e',
+            border: 'none',
+          },
+        }}
+      >
+        Subscribe
+      </Button>
+      <A href="https://twitter.com/atomarranger">
+        Or follow me on Twitter for more stuff like this.
+      </A>
+    </form>
+  )
+}
+
+export const Bio = () => {
+  return (
+    <Container
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'stretch',
+      }}
+    >
+      <div sx={{ display: 'flex' }}>
+        <ProfilePicture src={ProfileImage} alt={`Dylan Vann`} />
+        <p sx={{ marginBottom: 100 }}>
+          <strong>Dylan Vann</strong>
+          <br />
+          Software developer living in Toronto 🇨🇦 (and sometimes NYC 🗽).
+          <br />
+          <Focus />
+        </p>
+      </div>
+      <SignUpForm />
+    </Container>
+  )
+}
---
+++ src/components/GitHubLink.tsx
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled'
+
+export const GitHubLink = styled('a')`
+  padding: 6px 12px;
+  display: inline-block;
+  color: #428bca !important;
+  border: 1px solid #428bca;
+  transition-property: all;
+  transition-duration: 0.2s;
+  cursor: pointer;
+  background-color: transparent;
+  text-decoration: none;
+  :hover {
+    color: #1f496e !important;
+    border-color: #1f496e;
+  }
+`
