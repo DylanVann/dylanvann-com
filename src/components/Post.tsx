f19fd61b0fed361bae9bf438514917a9a53c10fa
Dylan Vann <dylan@dylanvann.com>
2020-08-26 00:05:30 -0400

    fix: improve meta tags (#33)

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -22,18 +22,35 @@ interface PostProps {
 export const Post = (props: PostProps) => {
   const title = get(props, 'frontmatter.title')
   const subtitle = get(props, 'frontmatter.subtitle')
+  const description = get(props, 'frontmatter.description')
   const date = get(props, 'fields.date')
   const datetime = get(props, 'fields.datetime')
   const slug = get(props, 'fields.slug')
   const github = get(props, 'frontmatter.github')
-  const ogImageUrl =
+  const baseUrl =
     process.env.CONTEXT === 'production'
-      ? `${process.env.URL}/${slug}og-image.png`
-      : `${process.env.DEPLOY_URL}/${slug}og-image.png`
+      ? process.env.URL
+      : process.env.DEPLOY_URL
+  const url = `${baseUrl}/${slug}`
+  const ogImageUrl = `${url}og-image.png`
   return (
     <div className={props.className} style={{ marginBottom: 80 }}>
       <Helmet>
+        {/* Generic */}
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:type" content="article" />
+        {/* Image */}
         <meta property="og:image" content={ogImageUrl} />
+        <meta property="og:image:alt" content={description} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        {/* Twitter */}
+        <meta name="twitter:site" content="@atomarranger" />
+        <meta name="twitter:creator" content="@atomarranger" />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:url" content={url} />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:image" content={ogImageUrl} />
       </Helmet>
