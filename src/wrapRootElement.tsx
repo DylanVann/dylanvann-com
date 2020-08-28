129cc5d88521d6ba529f524dcd1bca25169bb215
Dylan Vann <dylan@dylanvann.com>
2020-08-28 13:58:20 -0400

    fix: fix type error

--- src/wrapRootElement.tsx
+++ src/wrapRootElement.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { MDXProvider } from '@mdx-js/react'
 import { mdxComponents } from './mdxComponents'
 
-export const wrapRootElement = ({ element }) => (
+export const wrapRootElement = ({ element }: { element: React.ReactNode }) => (
   <React.StrictMode>
     <MDXProvider components={mdxComponents}>{element}</MDXProvider>
   </React.StrictMode>
