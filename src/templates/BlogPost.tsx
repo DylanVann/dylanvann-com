4ee951223deba3caf0f1f5380025f9bd92cac85a
Dylan Vann <dylan@dylanvann.com>
2020-09-08 20:48:47 -0400

    fix og images

--- src/templates/BlogPost.tsx
+++ src/templates/BlogPost.tsx
@@ -5,13 +5,40 @@ import { Bio } from '../components/Bio'
 import { Container } from '../components/Container'
 import { Layout } from '../components/Layout'
 import { Post } from '../components/Post'
+import { Helmet } from 'react-helmet-async'
 
 const BlogPostTemplate = (props: { location: any; description?: string }) => {
   const post = get(props, 'data.mdx')
   const title = get(post, 'frontmatter.title')
   const description = get(post, 'frontmatter.description')
+  const slug = get(props, 'fields.slug')
+  const baseUrl =
+    process.env.CONTEXT === 'production'
+      ? process.env.URL
+      : process.env.DEPLOY_URL
+  const url = `${baseUrl}/${slug}`
+  const ogImageUrl = `${url}/og-image.png`
   return (
     <Layout {...props} title={title} description={description}>
+      <Helmet>
+        {/* Generic */}
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:type" content="article" />
+        {/* Image */}
+        <meta property="og:image" content={ogImageUrl} />
+        <meta property="og:image:alt" content={description} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        {/* Twitter */}
+        <meta name="twitter:site" content="@atomarranger" />
+        <meta name="twitter:creator" content="@atomarranger" />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:url" content={url} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content={ogImageUrl} />
+      </Helmet>
       <Container>
         <Post {...post} />
         <hr />
